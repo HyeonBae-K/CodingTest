63d8f1f3b6fced8a0a4e4bb6319efc85a199bf5a
HyeonBae-K <gusqo14@gmail.com>
2021-02-25 17:27:16 +0900

    coding_Test

---
+++ BAEKJOON/11051_이항계수2_파스칼삼각형.py
@@ -0,0 +1,12 @@
+# 이항계수2 파스칼 삼각형
+n, k = map(int, input().split())
+dp = []
+
+for i in range(n+1):
+    dp.append([1]*(i+1))  # 1로 삼각형 모양 만들기
+
+for i in range(2, n+1):
+    for j in range(1, i):
+        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007
+
+print(dp[n][k])
